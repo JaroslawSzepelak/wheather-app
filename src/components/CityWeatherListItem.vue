69e675a64f527a28622bfd591be599ae23bc5976
Glorfindelus <szepelakjaroslaw@gmail.com>
2022-06-20 15:31:57 +0200

    dodanie wykresu, dodanie przęłączania się między wykresem a tabelą, dodanie aktualizacji wykresu/tabeli bez konieczności ich zamykania

--- src/components/CityWeatherListItem.vue
+++ src/components/CityWeatherListItem.vue
@@ -26,8 +26,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="title">
-              Tabela temperatury i wilgotności miasta {{ cityWeather.name }} w
-              czasie
+              Temperatura i wilgotność miasta {{ cityWeather.name }} w czasie
             </h5>
             <button
               type="button"
@@ -39,30 +38,52 @@
             </button>
           </div>
           <div class="modal-body">
-            <table class="table table-sm table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">Temperatura</th>
-                  <th scope="col">Wilgotność</th>
-                  <th scope="col">Czas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="m in measurements" v-bind:key="m.id">
-                  <td>{{ m.temp.toFixed(0) }}</td>
-                  <td>{{ m.humidity }}</td>
-                  <td>
-                    {{
-                      addLeadingZeros(m.time.getHours(), 2) +
-                      ":" +
-                      addLeadingZeros(m.time.getMinutes(), 2) +
-                      ":" +
-                      addLeadingZeros(m.time.getSeconds(), 2)
-                    }}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div class="container">
+              <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#chart"
+                    >Wykres</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#table">Tabela</a>
+                </li>
+              </ul>
+
+              <div class="tab-content">
+                <div id="chart" class="tab-pane fade show active">
+                  <h3>Wykres</h3>
+                  <line-chart :data="chartData"></line-chart>
+                </div>
+                <div id="table" class="tab-pane fade">
+                  <h3>Tabela</h3>
+                  <table class="table table-sm table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">Temperatura</th>
+                        <th scope="col">Wilgotność</th>
+                        <th scope="col">Czas</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="m in measurements" v-bind:key="m.id">
+                        <td>{{ m.temp.toFixed(0) }}</td>
+                        <td>{{ m.humidity }}</td>
+                        <td>
+                          {{
+                            addLeadingZeros(m.time.getHours(), 2) +
+                            ":" +
+                            addLeadingZeros(m.time.getMinutes(), 2) +
+                            ":" +
+                            addLeadingZeros(m.time.getSeconds(), 2)
+                          }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -71,6 +92,7 @@
 </template>
 
 <script lang="ts">
+import { ChartData } from "../model/ChartData";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { ListItem } from "../model/CityWeather";
 import { Measurement } from "../model/Measurement";
@@ -80,9 +102,12 @@ export default class CityWeatherListItem extends Vue {
   @Prop() public cityWeather!: ListItem;
   public measurements: Measurement[] = [];
   public popUpId = "";
+  public chartData: ChartData[] = [];
 
   public created() {
-    this.loadData();
+    window.setInterval(async () => {
+      this.loadData();
+    }, 60000);
     this.popUpId = "CityWeatherModal" + this.cityWeather.id;
   }
 
@@ -92,6 +117,28 @@ export default class CityWeatherListItem extends Vue {
         (element: Measurement) => element.cityId == this.cityWeather.id
       );
     }
+
+    this.chartData = [];
+
+    var temperatures = Object.assign(
+      {},
+      ...this.measurements.map((x) => ({ [x.time.toString()]: x.temp }))
+    );
+
+    var huminidities = Object.assign(
+      {},
+      ...this.measurements.map((x) => ({ [x.time.toString()]: x.humidity }))
+    );
+
+    this.chartData.push({
+      name: "Temperatura",
+      data: temperatures,
+    } as ChartData);
+
+    this.chartData.push({
+      name: "Wilgotność",
+      data: huminidities,
+    } as ChartData);
   }
 
   public addLeadingZeros(num: number, totalLength: number): string {
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
 import VueSession from "vue-session";
-
-Vue.use(VueSession);
-Vue.config.productionTip = false;
+import { Chart } from "chart.js";
+import Chartkick from "vue-chartkick";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+
+Vue.use(VueSession);
+Vue.use(Chartkick.use(Chart));
+Vue.config.productionTip = false;
+
 new Vue({
   router,
   store,
